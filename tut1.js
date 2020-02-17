0584cb75c169566910e7d7def2d66a2bff60f59c
an1020nmhk@gmail.com <an1020nmhk@gmail.com>
2020-02-17 07:06:11 -0800

    this our initialization of the files

---
+++ tut1.js
@@ -0,0 +1,12 @@
+console.time("myIdentifier");
+console.log("hello world!");
+console.table({name: "anuj Kumarsla", age: 18, class : "BCA 1yr",hobby: "programmer"
+});
+
+console.warn("this is your warning! ");
+// console.clear();
+console.timeEnd("myIdentifier");
+age=10;
+console.assert(age>18,"You are not capable to vote");
+//  if (condition is false than it executes ) then it prents error in the console
+console.error("this is my own error");
